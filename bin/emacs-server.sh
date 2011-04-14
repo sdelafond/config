f7e16e92402e6d7e407d6a7dd0fd4ed979fd8304
Sébastien Delafond <seb@untangle.com>
2011-04-14 13:40:28 +0200

    [emacs] bin/jed (emacs-client) now compatible with tmux.

---
+++ bin/emacs-server.sh
@@ -0,0 +1,26 @@
+#! /bin/sh
+
+# Copyright (C) 2008 Sebastien Delafond (sdelafond@gmail.com)
+# Author: Sebastien Delafond (sdelafond@gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+if [[ -n "$TMUX" ]] ; then
+  MARKER=$(echo $TMUX | awk -F, '{print $2}')
+else
+  MARKER=$STY
+fi
+
+emacs -nw --eval '(progn
+                  (setq server-name "'$MARKER'")
+                  (server-start)
+                  (shell-command "echo $WINDOW > $SERVER_WINDOW_FILE")
+                  (add-hook '\''kill-emacs-hook (lambda() (delete-file (getenv "SERVER_WINDOW_FILE")))))'
