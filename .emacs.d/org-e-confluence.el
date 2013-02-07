934b8bb53e66a028849b021315d4fef4eb4b7fc3
Sébastien Delafond <sdelafond@gmail.com>
2013-02-07 11:30:59 +0100

    [emacs] Latest org-e-confluence.el

--- .emacs.d/org-e-confluence.el
+++ .emacs.d/org-e-confluence.el
@@ -1,3 +1,25 @@
+;;; org-e-confluence --- Confluence Wiki Back-End for Org Export Engine
+
+;; Copyright (C) 2012 Sébastien Delafond
+
+;; Author: Sébastien Delafond <sdelafond at gmx dot net>
+;; Keywords: outlines, confluence, wiki
+
+;; This file is not part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; org-confluence.el lets you convert Org files to confluence files using
@@ -6,7 +28,8 @@
 ;; Put this file into your load-path and the following into your ~/.emacs:
 ;;	 (require 'org-confluence)
 ;;
-;; Export Org files to confluence: M-x org-e-confluence-export-as-confluence RET
+;; Export Org files to confluence:
+;; M-x org-e-confluence-export-as-confluence RET
 ;;
 ;;; Code:
 
@@ -43,7 +66,7 @@
   ;; FIXME: provide a user-controlled variable for theme
   (let ((content (org-export-format-code-default example-block info)))
     (org-e-confluence--block "none" "Confluence" content)))
-              
+
 (defun org-e-confluence-italic (italic contents info)
   (format "_%s_" contents))
 
@@ -57,7 +80,7 @@
         (level (org-export-get-relative-level headline info)))
     ;; Else: Standard headline.
     (format "h%s. %s\n%s" level text
-            (if (org-string-nw-p contents) contents 
+            (if (org-string-nw-p contents) contents
               ""))))
 
 (defun org-e-confluence-link (link desc info)
@@ -88,7 +111,7 @@
   contents)
 
 (defun org-e-confluence-table-row  (table-row contents info)
-  (concat 
+  (concat
    (if (org-string-nw-p contents) (format "|%s" contents)
      "")
    (when (org-export-table-row-ends-header-p table-row info)
@@ -117,13 +140,17 @@
 
 ;; main interactive entrypoint
 (defun org-e-confluence-export-as-confluence
-  (&optional subtreep visible-only body-only ext-plist)
+  (&optional async subtreep visible-only body-only ext-plist)
   "Export current buffer to a text buffer.
 
 If narrowing is active in the current buffer, only export its
 narrowed part.
 
 If a region is active, export that region.
+
+A non-nil optional argument ASYNC means the process should happen
+asynchronously.  The resulting buffer should be accessible
+through the `org-export-stack' interface.
 
 When optional argument SUBTREEP is non-nil, export the sub-tree
 at point, extracting information from the headline properties
@@ -143,11 +170,22 @@ Export is done in a buffer named \"*Org E-Confluence Export*\", which
 will be displayed when `org-export-show-temporary-export-buffer'
 is non-nil."
   (interactive)
-  (let ((outbuf (org-export-to-buffer
-		 'e-confluence "*Org E-Confluence Export*"
-		 subtreep visible-only body-only ext-plist)))
-    (with-current-buffer outbuf (text-mode))
-    (when org-export-show-temporary-export-buffer
-      (switch-to-buffer-other-window outbuf))))
+  (if async
+      (org-export-async-start
+	  (lambda (output)
+	    (with-current-buffer (get-buffer-create "*Org E-Confluence Export*")
+	      (erase-buffer)
+	      (insert output)
+	      (goto-char (point-min))
+	      (text-mode)
+	      (org-export-add-to-stack (current-buffer) 'e-confluence)))
+	`(org-export-as 'e-confluence ,subtreep ,visible-only ,body-only
+			',ext-plist))
+    (let ((outbuf (org-export-to-buffer
+		   'e-confluence "*Org E-Confluence Export*"
+		   subtreep visible-only body-only ext-plist)))
+      (with-current-buffer outbuf (text-mode))
+      (when org-export-show-temporary-export-buffer
+	(switch-to-buffer-other-window outbuf)))))
 
 (provide 'org-e-confluence)
