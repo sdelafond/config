f06b0cc878c878b47a94f2238fdcf77f43081004
Sébastien Delafond <sdelafond@gmail.com>
2013-09-07 03:45:52 +0200

    [emacs] Upgrade all ELPA packages

--- .emacs.d/elpa/anything-git-files-20130608.1711/anything-git-files-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "anything-git-files" "20130608.1711" "anything for git files [github]" (quote ((anything "1.3.9"))))
---
+++ .emacs.d/elpa/anything-git-files-20130609.1843/anything-git-files-pkg.el
@@ -0,0 +1,1 @@
+(define-package "anything-git-files" "20130609.1843" "anything for git files [github]" (quote ((anything "1.3.9"))))
