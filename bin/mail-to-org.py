05d3f4cbbf1b29f1a22b5f5e28dd3edcc62c88de
Sébastien Delafond <sdelafond@gmail.com>
2013-09-16 10:40:07 +0200

    [mail-to-org] jed alias is gone

--- bin/mail-to-org.py
+++ bin/mail-to-org.py
@@ -7,4 +7,4 @@ msg = email.message_from_string(sys.stdin.read())
 msgId = re.sub(r'[<>]', '', msg.get('message-id'))
 subject = msg.get('Subject')
 
-os.system("jed org-protocol:/capture:/m/mutt:%s/mail/'%s'" % (msgId, subject))
+os.system("emacs-wrapper.sh org-protocol:/capture:/m/mutt:%s/mail/'%s'" % (msgId, subject))
