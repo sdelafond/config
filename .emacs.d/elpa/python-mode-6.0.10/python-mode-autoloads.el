965fc1770fb656e8ecb1259ed9ecdba129cc37cc
Sébastien Delafond <sdelafond@gmail.com>
2014-10-21 09:58:52 +0200

    [emacs] Recent packages updates

--- .emacs.d/elpa/python-mode-6.0.10/python-mode-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; python-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (20915 44845 690974 211000))
-
-;;;***
-
-(provide 'python-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; python-mode-autoloads.el ends here
