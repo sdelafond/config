8734785bd2f37fb46f61a3d6a29b72d97d998f6b
Sébastien Delafond <sdelafond@gmail.com>
2017-08-08 21:35:00 -0400

    [xbacklight.sh] Refactor a bit

--- bin/xbacklight.sh
+++ bin/xbacklight.sh
@@ -3,9 +3,11 @@
 DIR=/sys/class/backlight/intel_backlight
 
 actual=$(cat $DIR/actual_brightness)
+
 case $1 in
-  -inc) new=$(( $actual + ( $2 * 10 ) )) ;;
-  -dec) new=$(( $actual - ( $2 * 10 ) )) ;;
+  -inc) op="+" ;;
+  -dec) op="-" ;;
 esac
 
+new=$(( $actual $op ( $2 * 2 ) ))
 echo $new | sudo tee $DIR/brightness
