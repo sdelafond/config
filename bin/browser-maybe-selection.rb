011e12f80b04f00f9002c38bf87e6dead3602a38
Sébastien Delafond <sdelafond@gmail.com>
2017-11-08 10:18:51 +0100

    [browser] Auto-decrypt clipboard content if needed

--- bin/browser-maybe-selection.rb
+++ bin/browser-maybe-selection.rb
@@ -3,32 +3,40 @@
 require 'shellwords'
 require 'uri'
 
+URL_SCHEMES = ['http', 'ftp', 'https']
+HTTP_REGEX = URI.regexp(URL_SCHEMES)
+
+# handle mutt wrap markers
+def open(selection, browser, mode)
+  selection.gsub!(/\n\+/m, '')
+
+  if mode == nil or mode == "default" then
+    case selection
+    when HTTP_REGEX
+      URI::extract(selection, URL_SCHEMES).each { |url|
+        url.gsub!(/\.$/, '')
+        system("#{browser} #{url.shellescape}")
+      }
+    end
+  elsif mode == 'gs' then
+    system("#{browser} 'http://www.google.com/search?q=#{selection}'")
+  elsif mode == 'gi' then
+    system("#{browser} 'http://www.google.com/images?q=#{selection}'")
+  elsif mode == 'gm' then
+    system("#{browser} 'http://maps.google.com/?q=#{selection}'")
+  end
+end
+
 #ENV['DISPLAY'] = ':'
 
-urlSchemes = ['http', 'ftp', 'https']
-
-system("pgrep -f '(firefox-bin|firefox.real)'")
+system("pgrep -f '(firefox-bin|firefox.real)' > /dev/null")
 browser = ($?.exitstatus == 0 && ARGV[1].nil?) ? "firefox" : "chromium"
 
 selection = `xclip -o`
 
-rhttp = URI.regexp(urlSchemes)
+open(selection, browser, ARGV[0])
 
-# handle mutt wrap markers
-selection.gsub!(/\n\+/m, '')
-
-if ARGV[0] == nil or ARGV[0] == "default" then
-  case selection
-  when rhttp
-    URI::extract(selection, urlSchemes).each { |url|
-      url.gsub!(/\.$/, '')
-      system("#{browser} #{url.shellescape}")
-    }
-  end
-elsif ARGV[0] == 'gs' then
-  system("#{browser} 'http://www.google.com/search?q=#{selection}'")
-elsif ARGV[0] == 'gi' then
-  system("#{browser} 'http://www.google.com/images?q=#{selection}'")
-elsif ARGV[0] == 'gm' then
-  system("#{browser} 'http://maps.google.com/?q=#{selection}'")
+if selection.index("BEGIN PGP MESSAGE") then
+  decoded = `echo '#{selection}' | gpg2 -d 2> /dev/null`.chomp
+  open(decoded, browser, ARGV[0]) if not decoded.empty?
 end
