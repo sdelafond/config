d6136157b3305f4934e18520ad2bf570e63a9590
Sébastien Delafond <sdelafond@gmail.com>
2017-11-15 08:21:51 +0100

    [bin] ip2city.py format

--- bin/ip2city.py
+++ bin/ip2city.py
@@ -1,6 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/python3
 
-import GeoIP, socket, sys
+import collections, GeoIP, socket, sys
+
+outputTpl = "%(host)s: %(city)s, %(region)s %(postal_code)s, %(country_name)s (%(country_code3)s) %(latitude).4f,%(longitude).4f"
 
 host = sys.argv[1]
 
@@ -16,10 +18,8 @@ if not h:
   print "couldn't locate host %s" % (host,)
   sys.exit(1)
 
+h = collections.defaultdict(str, **h)
 h['host'] = host
 
-for k,v in h.iteritems():
-  if not v:
-    h[k] = ''
-
-print ("%(host)s: %(city)s, %(region)s %(postal_code)s, %(country_name)s (%(country_code3)s, %(latitude)s, %(longitude)s " % h).decode('latin-1').encode('utf-8')
+print(h)
+print(outputTpl % h)
