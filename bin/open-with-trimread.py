9a3fdc879064e4422a4e4d14cf643e6230d21115
Sébastien Delafond <sdelafond@gmail.com>
2020-01-28 15:40:06 +0100

    mutt & open-with-trimread.py

---
+++ bin/open-with-trimread.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+import re
+import sys
+
+# constants
+URL = 'https://beta.trimread.com'
+
+## main
+payload = {'url': sys.argv[1]}
+
+resp = requests.post(URL, data=payload)
+
+if not resp.status_code == 200:
+    sys.exit(1)
+
+link = re.search(fr'({URL}/articles/\d+)', resp.text).group(1)
+print(link)
+
+
