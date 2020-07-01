676f62cc570292522884156d12f4d4bd93e175fb
Sébastien Delafond <sdelafond@gmail.com>
2020-07-01 08:14:10 +0200

    emacs/color: ledger

--- .emacs.d/lisp/color-theme-seb.el
+++ .emacs.d/lisp/color-theme-seb.el
@@ -35,6 +35,7 @@ widget, custom, latex, ediff."
      (info-xref ((t (:foreground "yellow" :bold t))))
      (ace-jump-face-foreground ((t (:foreground "yellow" :bold t))))
      (diary-face ((t (:foreground "orange"))))
+     (ledger-occur-xact-face ((t (:foreground "orange"))))
      (calendar-today-face ((t (:underline t))))
      (company-tooltip ((t (:background "color-249" :foreground "black"))))
      (makefile-space-face ((t (:background "hotpink"))))
