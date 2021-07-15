fb46b1ecdc1ee3f53221fce2e2f7cd2ead3bd0df
Sébastien Delafond <sdelafond@gmail.com>
2021-07-15 13:14:03 +0200

    i3: inject HOSTNAME into env

--- bin/i3-write.py
+++ bin/i3-write.py
@@ -3,20 +3,22 @@
 import jinja2
 import os
 import os.path
+import platform
 import sys
 
 HOME = os.getenv('HOME')
 
 if len(sys.argv) > 1:
-  I3_CONFIG = sys.argv[1]
+    I3_CONFIG = sys.argv[1]
 else:
-  I3_CONFIG = os.path.join(HOME, '.config/i3/config')
+    I3_CONFIG = os.path.join(HOME, '.config/i3/config')
 I3_CONFIG_TPL = I3_CONFIG + '.j2'
 
-
 if __name__ == '__main__':
-  with open(I3_CONFIG_TPL) as tpl:
-    template = jinja2.Template(tpl.read(), trim_blocks=True, lstrip_blocks=True)
-    result = template.render({'env': os.environ})
-    with open(I3_CONFIG, 'w') as conf:
-      conf.write(result)
+    with open(I3_CONFIG_TPL) as tpl:
+        template = jinja2.Template(tpl.read(), trim_blocks=True, lstrip_blocks=True)
+        env = os.environ
+        env['HOSTNAME'] = platform.node().split('.', 1)[0]
+        result = template.render({'env': env})
+        with open(I3_CONFIG, 'w') as conf:
+            conf.write(result)
