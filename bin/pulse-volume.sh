8337508f58ea8d061b9fb7b2ae5f64e3d7a18e73
Sébastien Delafond <sdelafond@gmail.com>
2021-11-03 15:40:20 +0100

    volume: handle 2-digit sink numbers

--- bin/pulse-volume.sh
+++ bin/pulse-volume.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 STEP=3
-SINK=$(pacmd list-sinks | perl -ne 'print $1 if m/\* index: (\d)/')
+SINK=$(pacmd list-sinks | perl -ne 'print $1 if m/\* index: (\d+)/')
 
 case $1 in
   up|down)
-    [ $1 = up ] && UP="+" || UP="-"
+    [ $1 = up ] && DIRECTION="+" || DIRECTION="-"
     pactl set-sink-mute $SINK 0
-    pactl set-sink-volume $SINK ${UP}${STEP}%
+    pactl set-sink-volume $SINK ${DIRECTION}${STEP}%
     STRING=$(pacmd dump-volumes | awk '/^Sink '${SINK}'/ {print $8}') ;;
   mute)
     pactl set-sink-mute $SINK 1
